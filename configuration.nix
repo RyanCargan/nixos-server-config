dc5cf55ff875ddc2f530da02d93179366934962b
Ryan Cargan <ryancargan@gmail.com>
2022-06-11 20:44:50 +0530

    Refactor Nginx location code
    Clean up config and remove non-essential options.

--- configuration.nix
+++ configuration.nix
@@ -49,8 +49,6 @@
 
     commonHttpConfig = ''
 
-    # index index.html;
-
     upstream frontend {
       server 127.0.0.1:3000;
       server 127.0.0.1:3010 backup;
@@ -63,32 +61,82 @@
 
     '';
 
-    # appendHttpConfig = ''
-    # ''
-
     virtualHosts = {
       "codinghermit.net" = {
         http2 = true;
         enableACME = true;
         forceSSL = true;
 
-        # locations."/" = {
-        #   root = "/var/www/static";
-        # };
-
         locations."/" = {
-          proxyPass = "http://frontend";
+          root = "/var/www/static";
           extraConfig = ''
-          
-          proxy_intercept_errors on;
-          error_page 404 = @fallback;
-          
+
+          etag on;
+          gzip on;
+
+          add_header 'Cross-Origin-Embedder-Policy' 'require-corp' always;
+          add_header 'Cross-Origin-Opener-Policy' 'same-origin' always;
+
+          add_header 'Access-Control-Allow-Origin' '*' always;
+          add_header 'Access-Control-Allow-Methods' 'POST, PUT, DELETE, GET, PATCH, OPTIONS' always;
+          add_header 'Access-Control-Allow-Headers' 'Authorization, Content-Type, Idempotency-Key' always;
+          add_header 'Access-Control-Expose-Headers' 'Link, X-RateLimit-Reset, X-RateLimit-Limit, X-RateLimit-Remaining, X-Request-Id' always;
+          if ($request_method = OPTIONS) {
+            return 204;
+          }
+
+          add_header X-XSS-Protection "1; mode=block";
+          add_header X-Permitted-Cross-Domain-Policies none;
+          add_header X-Frame-Options DENY;
+          add_header X-Content-Type-Options nosniff;
+          add_header Referrer-Policy same-origin;
+          add_header X-Download-Options noopen;
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "upgrade";
+          proxy_set_header Host $host;
+
+          client_max_body_size 16m;
+
           '';
         };
 
-        locations."@fallback" = {
-          root = "/var/www/static";
-          tryFiles = "$uri =404";
+        locations."/dev" = {
+          proxyPass = "http://frontend";
+          extraConfig = ''
+
+          etag on;
+          gzip on;
+
+          add_header 'Cross-Origin-Embedder-Policy' 'require-corp' always;
+          add_header 'Cross-Origin-Opener-Policy' 'same-origin' always;
+
+          add_header 'Access-Control-Allow-Origin' '*' always;
+          add_header 'Access-Control-Allow-Methods' 'POST, PUT, DELETE, GET, PATCH, OPTIONS' always;
+          add_header 'Access-Control-Allow-Headers' 'Authorization, Content-Type, Idempotency-Key' always;
+          add_header 'Access-Control-Expose-Headers' 'Link, X-RateLimit-Reset, X-RateLimit-Limit, X-RateLimit-Remaining, X-Request-Id' always;
+          if ($request_method = OPTIONS) {
+            return 204;
+          }
+
+          add_header X-XSS-Protection "1; mode=block";
+          add_header X-Permitted-Cross-Domain-Policies none;
+          add_header X-Frame-Options DENY;
+          add_header X-Content-Type-Options nosniff;
+          add_header Referrer-Policy same-origin;
+          add_header X-Download-Options noopen;
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "upgrade";
+          proxy_set_header Host $host;
+
+          client_max_body_size 16m;
+
+          '';
+        };
+
+          locations."/api" = {
+          proxyPass = "http://backend";
           extraConfig = ''
 
           etag on;
