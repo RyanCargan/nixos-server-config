62cf2e540a35ff95e56e2cdc0b9db278a55142a6
Ryan Cargan <ryancargan@gmail.com>
2022-06-11 18:00:24 +0530

    Refactor root attribute for Nginx

--- configuration.nix
+++ configuration.nix
@@ -72,53 +72,54 @@
         enableACME = true;
         forceSSL = true;
 
-      locations."/" = {
-        root = "/var/www/static";
-      };
-
         # locations."/" = {
-        #   proxyPass = "http://frontend";
-        #   extraConfig = ''
-          
-        #   proxy_intercept_errors on;
-        #   error_page 404 = @fallback;
-          
-        #   '';
+        #   root = "/var/www/static";
         # };
 
-        # locations."@fallback" = {
-        #   tryFiles = "$uri =404";
-        #   extraConfig = ''
+        locations."/" = {
+          proxyPass = "http://frontend";
+          extraConfig = ''
+          
+          proxy_intercept_errors on;
+          error_page 404 = @fallback;
+          
+          '';
+        };
 
-        #   etag on;
-        #   gzip on;
+        locations."@fallback" = {
+          root = "/var/www/static";
+          tryFiles = "$uri =404";
+          extraConfig = ''
 
-        #   add_header 'Cross-Origin-Embedder-Policy' 'require-corp' always;
-        #   add_header 'Cross-Origin-Opener-Policy' 'same-origin' always;
+          etag on;
+          gzip on;
 
-        #   add_header 'Access-Control-Allow-Origin' '*' always;
-        #   add_header 'Access-Control-Allow-Methods' 'POST, PUT, DELETE, GET, PATCH, OPTIONS' always;
-        #   add_header 'Access-Control-Allow-Headers' 'Authorization, Content-Type, Idempotency-Key' always;
-        #   add_header 'Access-Control-Expose-Headers' 'Link, X-RateLimit-Reset, X-RateLimit-Limit, X-RateLimit-Remaining, X-Request-Id' always;
-        #   if ($request_method = OPTIONS) {
-        #     return 204;
-        #   }
+          add_header 'Cross-Origin-Embedder-Policy' 'require-corp' always;
+          add_header 'Cross-Origin-Opener-Policy' 'same-origin' always;
 
-        #   add_header X-XSS-Protection "1; mode=block";
-        #   add_header X-Permitted-Cross-Domain-Policies none;
-        #   add_header X-Frame-Options DENY;
-        #   add_header X-Content-Type-Options nosniff;
-        #   add_header Referrer-Policy same-origin;
-        #   add_header X-Download-Options noopen;
-        #   proxy_http_version 1.1;
-        #   proxy_set_header Upgrade $http_upgrade;
-        #   proxy_set_header Connection "upgrade";
-        #   proxy_set_header Host $host;
+          add_header 'Access-Control-Allow-Origin' '*' always;
+          add_header 'Access-Control-Allow-Methods' 'POST, PUT, DELETE, GET, PATCH, OPTIONS' always;
+          add_header 'Access-Control-Allow-Headers' 'Authorization, Content-Type, Idempotency-Key' always;
+          add_header 'Access-Control-Expose-Headers' 'Link, X-RateLimit-Reset, X-RateLimit-Limit, X-RateLimit-Remaining, X-Request-Id' always;
+          if ($request_method = OPTIONS) {
+            return 204;
+          }
 
-        #   client_max_body_size 16m;
+          add_header X-XSS-Protection "1; mode=block";
+          add_header X-Permitted-Cross-Domain-Policies none;
+          add_header X-Frame-Options DENY;
+          add_header X-Content-Type-Options nosniff;
+          add_header Referrer-Policy same-origin;
+          add_header X-Download-Options noopen;
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "upgrade";
+          proxy_set_header Host $host;
 
-        #   '';
-        # };
+          client_max_body_size 16m;
+
+          '';
+        };
       };
     };
   };
