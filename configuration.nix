2af306d68bfd0d8f792bb30c7c03f27693ddb9c6
Ryan Cargan <ryancargan@gmail.com>
2022-06-12 11:25:02 +0530

    Add ci server to Nginx

--- configuration.nix
+++ configuration.nix
@@ -59,6 +59,11 @@
       server 127.0.0.1:4010 backup;
     }
 
+    upstream ci {
+      server 127.0.0.1:5000;
+      server 127.0.0.1:5010 backup;
+    }
+
     '';
 
     virtualHosts = {
@@ -135,8 +140,42 @@
           '';
         };
 
-          locations."/api" = {
+        locations."/api" = {
           proxyPass = "http://backend";
+          extraConfig = ''
+
+          etag on;
+          gzip on;
+
+          add_header 'Cross-Origin-Embedder-Policy' 'require-corp' always;
+          add_header 'Cross-Origin-Opener-Policy' 'same-origin' always;
+
+          add_header 'Access-Control-Allow-Origin' '*' always;
+          add_header 'Access-Control-Allow-Methods' 'POST, PUT, DELETE, GET, PATCH, OPTIONS' always;
+          add_header 'Access-Control-Allow-Headers' 'Authorization, Content-Type, Idempotency-Key' always;
+          add_header 'Access-Control-Expose-Headers' 'Link, X-RateLimit-Reset, X-RateLimit-Limit, X-RateLimit-Remaining, X-Request-Id' always;
+          if ($request_method = OPTIONS) {
+            return 204;
+          }
+
+          add_header X-XSS-Protection "1; mode=block";
+          add_header X-Permitted-Cross-Domain-Policies none;
+          add_header X-Frame-Options DENY;
+          add_header X-Content-Type-Options nosniff;
+          add_header Referrer-Policy same-origin;
+          add_header X-Download-Options noopen;
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "upgrade";
+          proxy_set_header Host $host;
+
+          client_max_body_size 16m;
+
+          '';
+        };
+
+        locations."/ci" = {
+          proxyPass = "http://ci";
           extraConfig = ''
 
           etag on;
