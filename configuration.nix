569dcad090ef74ba9638e764c7761f6378b90604
Ryan Cargan <ryancargan@gmail.com>
2022-06-14 11:06:46 +0530

    Add proxy bugfix test

--- configuration.nix
+++ configuration.nix
@@ -210,6 +210,9 @@
 
           # Redirect all HTTP traffic to proxy_pass
           proxy_set_header X-Real-IP $remote_addr;
+          proxy_redirect         off;
+          proxy_set_header X-NginX-Proxy true;
+
           proxy_set_header Host $host;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
 
