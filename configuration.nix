a4dc2cf0cfff4293a1bfe2fb5eca043e1fd70817
Ryan Cargan <ryancargan@gmail.com>
2022-06-18 22:06:55 +0530

    Remove timeout changes

--- configuration.nix
+++ configuration.nix
@@ -63,9 +63,9 @@
       server 127.0.0.1:4001;
       server 127.0.0.1:4011 backup;
 
-      proxy_read_timeout 1h;
-      proxy_connect_timeout 1h;
-      proxy_send_timeout 1h;
+      # proxy_read_timeout 3600;
+      # proxy_connect_timeout 3600;
+      # proxy_send_timeout 3600;
     }
 
     upstream ci {
